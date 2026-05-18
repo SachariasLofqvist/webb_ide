89b46a6eecb613c52a15e1da95dda6f875e849c1
SachariasLofvist <lofqvist.sacharias@gmail.com>
2026-05-18 13:46:44 +0200

    Added Connection to database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
 const app = express();
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080")
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
 });
